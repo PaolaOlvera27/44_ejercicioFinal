85e16b170b41ac44a1dfdc9b1abbba6f7a59de99
PaolaOlvera27 <apao.iq1@gmail.com>
2016-12-06 11:28:06 -0600

    Etapa 9

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -6,8 +6,12 @@ $(document).ready( function(){
 //ocultar y mostrar recetas
 	$(".js-show-make").click(function(){
 	  $(".page").addClass("make");
+		$(".js-show-make").addClass("active");
+		$(".js-show-recipe").removeClass("active");
 	});
 	$(".js-show-recipe").click(function(){
 		$(".page").removeClass("make");
+		$(".js-show-recipe").addClass("active");
+		$(".js-show-make").removeClass("active");
 	});
 });
