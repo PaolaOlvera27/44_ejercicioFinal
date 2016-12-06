ee78385ca096d3f788f469f854853deaef0e8f49
PaolaOlvera27 <apao.iq1@gmail.com>
2016-12-06 11:24:23 -0600

    Etapa 8

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -2,4 +2,12 @@ $(document).ready( function(){
 	console.log("Se ha cargado el documento");
 //ocultar menu
 	$(".js-menu").hide();
+
+//ocultar y mostrar recetas
+	$(".js-show-make").click(function(){
+	  $(".page").addClass("make");
+	});
+	$(".js-show-recipe").click(function(){
+		$(".page").removeClass("make");
+	});
 });
