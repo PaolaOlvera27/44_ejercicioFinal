77831e8f87f3cf733bcc5885bbe51da49dee4004
PaolaOlvera27 <apao.iq1@gmail.com>
2016-12-06 11:09:15 -0600

    Etapa 4

--- js/script-index.js
+++ js/script-index.js
@@ -2,6 +2,8 @@ $(document).ready( function(){
 	printNew();
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
+	renderRecipe();
+
 
 	// ocultar flecha menu
 	$(".js-back").hide();
@@ -18,7 +20,13 @@ function printNew (){
 */
 function renderHighlightedRecipes(recipesArray) {
 	console.log('Recipes: ', recipesArray);
+	for (var i=0; i<recipesArray.length; i++) {
+		if(recipesArray[i].highlighted == true) {
+				renderRecipe(recipesArray[i]);
+		}
+	}
 }
+
 
 /*
 * Función que se encarga de pintar UNA recetas que tenga
@@ -27,7 +35,48 @@ function renderHighlightedRecipes(recipesArray) {
 * archivo "templates/templates-recipe.html"
 */
 function renderRecipe(recipe) {
-	console.log('Voy a pintar la receta: ', recipe);
+	console.log('Voy a pintar la receta : ', recipe);
+	if (!recipe) {
+		return;
+	}
+
+	var aRecipe = $('<a></a>');
+	aRecipe.addClass("item-recipe");
+
+	var spanAttr = $('<span></span>');
+	spanAttr.addClass("attribution");
+
+	var spanTitle = $('<span></span>');
+	spanTitle.addClass("title-recipe");
+	console.log(recipe.title);
+	spanTitle.text(recipe.title);
+
+	var spanMetadata = $('<span></span>');
+	spanMetadata.addClass("metadata-recipe");
+
+	var spanAuthor = $('<span></span>');
+	spanAuthor.addClass("author-recipe");
+	spanAuthor.text(recipe.name);
+
+	var spanBook = $('<span></span>');
+  spanBook.addClass("bookmarks-recipe");
+
+	var spanIcon = $('<span></span>');
+  spanIcon.addClass("icon-bookmark");
+
+	var imgRecipe = $('<img />');
+  imgRecipe.attr("src", "img/recipes/320x350/" + recipe.name + ".jpg" );
+
+	aRecipe.append(spanAttr);
+	aRecipe.append(imgRecipe);
+	spanAttr.append(spanTitle);
+	spanAttr.append(spanMetadata);
+	spanMetadata.append(spanAuthor);
+	spanMetadata.append(spanBook);
+	spanBook.append(spanIcon);
+
+	$('.list-recipes').append(aRecipe);
+
 }
 
 
